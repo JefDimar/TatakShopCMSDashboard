a322c2f1cea4c92fe825787fde647243b02e78e6
jefDimar <jefdimar@gmail.com>
2021-02-16 10:50:23 +0700

    init client,, start working on layouting

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: linear-gradient(to right, #373B44, #4286f4);
+}
+</style>
